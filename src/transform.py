d9e413646bc351bcf28a3e3673ea4bc5d0bf8fc2
abhiiiiiiijit <abhijitjan22@gmail.com>
2025-01-12 21:49:10 +0100

    Downloaded a dataset of approx 800MB and read and tested by pyspark.

--- src/transform.py
+++ src/transform.py
@@ -10,15 +10,17 @@ data = [("Alice", 28), ("Bob", 32), ("Cathy", 25)]
 columns = ["Name", "Age"]
 
 # Step 3: Create a DataFrame
-df = spark.createDataFrame(data, columns)
+# df = spark.createDataFrame(data, columns)
+df = spark.read.csv("/home/adminabhi/gitrepo/marathon_dataset/marathon_dataset.csv", header=True, inferSchema=True)
 
 # Step 4: Show the DataFrame
 print("Original DataFrame:")
-df.show()
+df.show(5)
 
 # Step 5: Perform a transformation (filtering ages > 30)
-filtered_df = df.filter(df.Age > 30)
+# filtered_df = df.filter(df.Age > 30)
+filtered_df = df.filter(df['Year of event'] == 1996)
 
 print("Filtered DataFrame (Age > 30):")
-filtered_df.show()
+filtered_df.show(5)
 
